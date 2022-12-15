039d884f57198b7b46d4f925f3f09194ed436687
pilyang <30036534+pilyang@users.noreply.github.com>
2022-12-15 16:12:47 +0900

    feat: 랜덤 다리 정보 생성하기
    - Tile.java enum 구현
     - 다리의 위치를 나타내는 enum
    - BridgeMaker.makeBridge: 사이즈에 맞추어 생성된 코드를 통해 문자열 리스트로 변환하여 반환

---
+++ src/main/java/bridge/domain/Tile.java
@@ -0,0 +1,31 @@
+package bridge.domain;
+
+import java.util.Arrays;
+
+public enum Tile {
+    UP("U", 1),
+    DOWN("D", 0);
+
+    private static final String CODE_EXCEPTION_MESSAGE = "[ERROR] 해당되는 코드의 다리타일 정보가 없습니다.";
+    private static final String DIRECTION_EXCEPTION_MESSAGE = "[ERROR] 해당되는 방향의 다리 정보가 없습니다.";
+
+    private final String direction;
+    private final int code;
+
+    Tile(String direction, int code) {
+        this.direction = direction;
+        this.code = code;
+    }
+
+    public static Tile findByCode(int code) {
+        return Arrays.stream(Tile.values())
+                .filter(tile -> tile.code == code)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(CODE_EXCEPTION_MESSAGE));
+    }
+
+
+    public String getDirection() {
+        return direction;
+    }
+}
